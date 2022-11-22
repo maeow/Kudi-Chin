e528e48798a94269d6efed9787f43e2046d3a214
chanikarn030 <73680793+chanikarn030@users.noreply.github.com>
2022-11-22 20:32:42 +0700

    Update historyScript.js

--- historyScript.js
+++ historyScript.js
@@ -1,4 +1,3 @@
-window.onload = codeAddress;
 var model = document.querySelectorAll('.model');
 var text = document.querySelectorAll('.text');
 var portrait = false;
@@ -19,12 +18,12 @@ function firstShow(portrait){
         fire[i].style.opacity = '1';
     }
     if(portrait){
-        model[1].style.top = '2vh';
-        model[2].style.bottom = '55vh';
+        model[1].style.top = '4vh';
+        model[2].style.top = '30vh';
         model[2].style.left = '2vw';
-        model[3].style.bottom = '35vh';
+        model[3].style.top = '51vh';
         model[3].style.right = '10vw';
-        text[0].style.top = '72vh'
+        text[0].style.top = '74vh'
     }
     else{
         model[1].style.top = '2vh';
@@ -41,9 +40,9 @@ function firstHide(scrollv, portrait){
     }
     if(portrait){
         model[1].style.top = '-30vh';
-        model[2].style.bottom = '55vh';
+        model[2].style.top = '30vh';
         model[2].style.left = '-58vw';
-        model[3].style.bottom = '35vh';
+        model[3].style.top = '51vh';
         model[3].style.right = '-58vw';
         if(scrollv>=height*10){
             text[0].style.top = '-15vh';
@@ -69,12 +68,13 @@ function firstHide(scrollv, portrait){
 function secondShow(portrait){
     model[5].style.right = '55vw';
     model[6].style.left = '55vw';
-    text[1].style.bottom = '10vh'
     if(portrait){
         model[4].style.top = '20vh';
+        text[1].style.top = '75vh'
     }
     else{
         model[4].style.top = '5vh';
+        text[1].style.top = '75vh'
     }
 }
 function secondHide(scrollv, portrait){
@@ -82,25 +82,25 @@ function secondHide(scrollv, portrait){
     model[5].style.right = '100vw';
     model[6].style.left = '100vw';
     if(scrollv>=height*16){
-        text[1].style.bottom = '120vh';
+        text[1].style.top = '-20vh';
     }
     else{
-        text[1].style.bottom = '-20vh';
+        text[1].style.top = '120vh';
     }
 }
 function thirdShow(portrait){
     if(portrait){
         model[7].style.right = '11vw';
-        model[7].style.bottom = '45vh';
-        model[8].style.bottom = '30vh';
+        model[7].style.top = '21vh';
+        model[8].style.top = '34vh';
         model[8].style.right = '0vw';
-        model[9].style.bottom = '50vh';
+        model[9].style.top = '20vh';
         model[9].style.right = '55vw';
         model[10].style.top = '4vh';
         model[10].style.right = '-23vw';
         model[11].style.top = '32vh';
         model[11].style.right = '64vw';
-        text[2].style.bottom = '15vh'
+        text[2].style.top = '80vh'
     }
     else{
         model[7].style.right = '68vw';
@@ -120,29 +120,29 @@ function thirdHide(scrollv, portrait){
     if(portrait){
         if(scrollv>=height*22){
             model[7].style.right = '100vw';
-            model[7].style.bottom = '45vh';
-            model[8].style.bottom = '30vh';
+            model[7].style.top = '21vh';
+            model[8].style.top = '34vh';
             model[8].style.right = '100vw';
-            model[9].style.bottom = '50vh';
+            model[9].style.top = '20vh';
             model[9].style.right = '100vw';
             model[10].style.top = '4vh';
             model[10].style.right = '100vw';
             model[11].style.top = '32vh';
             model[11].style.right = '100vw';
-            text[2].style.bottom = '120vh'
+            text[2].style.top = '-20vh'
         }
         else{
             model[7].style.right = '-71vw';
-            model[7].style.bottom = '42vh';
-            model[8].style.bottom = '28vh';
+            model[7].style.top = '23vh';
+            model[8].style.top = '45vh';
             model[8].style.right = '-70vw';
-            model[9].style.bottom = '45vh';
+            model[9].style.top = '25vh';
             model[9].style.right = '-55vw';
             model[10].style.top = '6vh';
             model[10].style.right = '-73vw';
             model[11].style.top = '34vh';
             model[11].style.right = '-54vw';
-            text[2].style.bottom = '-10vh'
+            text[2].style.top = '120vh'
         }
     }
     else{
@@ -178,7 +178,7 @@ function forthShow(portrait){
    if(portrait){
         model[12].style.right = '20vw';
         model[13].style.bottom = '0vh';
-        text[3].style.top = '10vh'
+        text[3].style.top = '20vh'
    }
    else{
         model[12].style.right = 'calc(50% - 16vmax)';
@@ -209,7 +209,7 @@ function forthHide(scrollv, portrait){
     }
 }
 function fifthShow(portrait){
-    text[4].style.bottom = '20vh'
+    text[4].style.top = '70vh'
     model[14].style.top = '20vh';
     model[14].style.right = '50vw';
     model[15].style.top = '20vh';
@@ -226,7 +226,7 @@ function fifthHide(scrollv, portrait){
             model[14].style.right = '63vw';
             model[15].style.top = '45vh';
             model[15].style.left = '18vw';
-            text[4].style.bottom = '120vh'
+            text[4].style.top = '-20vh'
             model[14].style.transform = 'scale(0.65)';
             model[15].style.transform = 'scale(0.65)';
             }
@@ -237,7 +237,7 @@ function fifthHide(scrollv, portrait){
             model[14].style.right = '100vw';
             model[15].style.top = '30vh';
             model[15].style.left = '100vw';
-            text[4].style.bottom = '-10vh'
+            text[4].style.top = '120vh'
         }
         else{
             // จบสุด
@@ -257,7 +257,7 @@ function fifthHide(scrollv, portrait){
             model[14].style.right = '63vw';
             model[15].style.top = '20vh';
             model[15].style.left = '15.5vw';
-            text[4].style.bottom = '120vh'
+            text[4].style.top = '-20vh'
             }
         }
         else if(scrollv<height*30){
@@ -266,7 +266,7 @@ function fifthHide(scrollv, portrait){
             model[14].style.right = '100vw';
             model[15].style.top = '20vh';
             model[15].style.left = '100vw';
-            text[4].style.bottom = '-10vh'
+            text[4].style.top = '100vh'
         }
         else{
             // จบสุด
@@ -281,16 +281,16 @@ function sixthShow(portrait){
     if(portrait){
         model[16].style.top = '28vh';
         model[16].style.left = '25vw';
-        model[17].style.bottom = '40vh';
+        model[17].style.top = '37vh';
         model[17].style.right = '10vw';
         model[18].style.top = '16vh';
         model[18].style.right = '10vw';
-        model[19].style.bottom = '30vh';
+        model[19].style.top = '50vh';
         model[19].style.right = '20vw';
         model[20].style.top = '15vh';
         model[20].style.left = '0vw';
         model[20].style.transform = 'none'
-        text[5].style.bottom = '10vh'
+        text[5].style.top = '80vh'
     }
     else{
         model[16].style.top = '18vh';
@@ -313,16 +313,16 @@ function sixthHide(scrollv, portrait){
             // เปลี่ยนหน้า
             model[16].style.top = '28vh';
             model[16].style.left = '100vw';
-            model[17].style.bottom = '25vh';
+            model[17].style.top = '40vh';
             model[17].style.right = '-30vw';
             model[18].style.top = '16vh';
             model[18].style.right = '100vw';
-            model[19].style.bottom = '38vh';
+            model[19].style.top = '47vh';
             model[19].style.right = '100vw';
             model[20].style.top = '30vh';
             model[20].style.left = 'calc(50% - 10.5vmax)';
             model[20].style.transform = 'scale(2)'
-            text[5].style.bottom = '120vh'
+            text[5].style.top = '-20vh'
             model[20].style.opacity = '1';
         }
         else if(scrollv>=height*46){
@@ -334,16 +334,16 @@ function sixthHide(scrollv, portrait){
             // เริ่มต้น
             model[16].style.top = '-30vh';
             model[16].style.left = '20vw';
-            model[17].style.bottom = '40vh';
+            model[17].style.top = '37vh';
             model[17].style.right = '-30vw';
             model[18].style.top = '-20vh';
             model[18].style.right = '-50vw';
-            model[19].style.bottom = '25vh';
+            model[19].style.top = '51vh';
             model[19].style.right = '-30vw';
             model[20].style.top = '-40vh';
             model[20].style.left = '35vw';
             model[20].style.transform = 'none'
-            text[5].style.bottom = '-10vh'
+            text[5].style.top = '120vh'
         }
     }
     else{
@@ -383,7 +383,7 @@ function sixthHide(scrollv, portrait){
 function seventhShow(portrait){
     if(portrait){
         text[6].style.opacity = '1';
-        text[6].style.bottom = '15vh'
+        text[6].style.top = '75vh'
     }
     else{
         text[6].style.bottom = '35vh'
@@ -419,73 +419,7 @@ function eighthHide(scrollv, portrait){
     model[21].style.top = '120vh';
     text[7].style.top = '120vh'
 }
-// alert(model[1].getAttribute('data-img'))
 
-function codeAddress(){
-    var scrollv = $(this).scrollTop();
-    // check แนวจอ
-    if(window.innerHeight > window.innerWidth){
-        portrait = true;
-    }
-    else{
-        portrait = false;
-    }
-    //
-
-    if(scrollv < height*4){
-        zeroShow(portrait);
-    }else{
-        zeroHide(portrait);
-    }
-    if(scrollv >= height*6 && scrollv < height*10){
-        firstShow(portrait);
-     }
-     else{
-        firstHide(scrollv, portrait);
-     }
-    if(scrollv >= height*12 && scrollv < height*16){
-       secondShow(portrait);
-    }
-    else{
-       secondHide(scrollv, portrait);
-    }
-    if(scrollv >= height*18 && scrollv < height*22){
-        thirdShow(portrait);
-    }
-    else{
-        thirdHide(scrollv, portrait);
-    }
-    if(scrollv >= height*24 && scrollv < height*28){
-        forthShow(portrait);
-    }
-    else{
-        forthHide(scrollv,  portrait);
-    }
-    if(scrollv >= height*30 && scrollv < height*36){
-        fifthShow(portrait);
-    }
-    else{
-        fifthHide(scrollv, portrait);
-    }
-    if(scrollv >= height*36 && scrollv < height*42){
-        sixthShow(portrait);
-    }
-    else{
-        sixthHide(scrollv, portrait);
-    }
-    if(scrollv >= height*42 && scrollv < height*46){
-        seventhShow(portrait);
-    }
-    else{
-        seventhHide(scrollv, portrait);
-    }
-    if(scrollv >= height*48){
-        eighthShow(portrait);
-    }
-    else{
-        eighthHide(scrollv, portrait);
-    }
-}
 
 $(window).scroll(function () {
     // check แนวจอ
